143e0bfee0fb73b1bf6acd02d5e0d37679c860ca
wave-or-corpuscula <ninzyago@gmail.com>
2025-11-03 15:12:42 +0300

    Prime number checher in rust done

---
+++ 03.11.25/Is-a-Number-Prime/src/main.rs
@@ -0,0 +1,41 @@
+/*Define a function that takes an integer argument and returns a logical value true or false depending on if the integer is a prime.
+
+Per Wikipedia, a prime number ( or a prime ) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+Requirements
+
+    You can assume you will be given an integer input.
+    You can not assume that the integer will be only positive. You may be given negative numbers as well ( or 0 ).
+    NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. 
+    Numbers go up to 2^31 ( or similar, depending on language ). Looping all the way up to n, or n/2, will be too slow.
+ */
+
+
+fn is_prime_beauty_but_slow(x: i64) -> bool {
+    if x <= 1 {
+        return false
+    }
+    (2..=x/2).all(|i| x % i != 0)
+}
+
+fn is_prime(x: i64) -> bool {
+    if x <= 1 {
+        return false;
+    }
+    if x == 2 || x == 3 {
+        return true;
+    }
+
+    let mut i = 5;
+    while i * i < x {
+        if (x % i == 0) || (x % (i + 2) == 0) {
+            return false
+        }
+        i += 6;
+    }
+
+    true
+}
+
+fn main() {
+    println!("{}", is_prime(4));
+}
