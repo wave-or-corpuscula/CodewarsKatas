faefde4b128e04cc01773bdaa1bc9d674651953b
wave-or-corpuscula <ninzyago@gmail.com>
2025-11-05 11:30:10 +0300

    Better version

--- 05.11.25/Decode-the-Morse-Code/src/main.rs
+++ 05.11.25/Decode-the-Morse-Code/src/main.rs
@@ -3,20 +3,17 @@ use std::collections::HashMap;
 
 fn decode_morse(encoded: &str) -> String {
 
-    // let mut MORSE_CODE: HashMap<String, String> = HashMap::new();
-    // MORSE_CODE.insert(".-".to_string(), "A".to_string());
-    // MORSE_CODE.insert("-...".to_string(), "B".to_string());
-    // MORSE_CODE.insert("-.-.".to_string(), "C".to_string());
+    let mut MORSE_CODE: HashMap<String, String> = HashMap::new();
+    MORSE_CODE.insert(".-".to_string(), "A".to_string());
+    MORSE_CODE.insert("-...".to_string(), "B".to_string());
+    MORSE_CODE.insert("-.-.".to_string(), "C".to_string());
 
     encoded
         .trim()
-        .split("   ")
-        .map(|word| {
-            word.split(' ')
-                .filter(|c| !c.is_empty())
-                .map(|code| MORSE_CODE[code].as_str())
-                .collect::<String>()
-        })
+        .split(' ')
+        .map(|x| {MORSE_CODE.get(x).unwrap_or(&" ".to_string()).clone()})
+        .collect::<String>()
+        .split_whitespace()
         .collect::<Vec<_>>()
         .join(" ")
 }
